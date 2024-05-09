f13aa686550dcfbdcec2efbe80ac64bb230ca60f
bolakecil <jeslynn30@gmail.com>
2024-05-09 22:09:41 +0700

    rest of the screen (prone to updates)
    - new asset for warning sign
    - new asset for save button

--- lib/screens/summary_screen.dart
+++ lib/screens/summary_screen.dart
@@ -2,8 +2,34 @@
 import 'package:flutter/material.dart';
 ////brisik bgt warningnya anjir
 
-class SummaryScreen extends StatelessWidget {
+class SummaryScreen extends StatefulWidget {
   @override
+  SummaryState createState() => SummaryState();
+}
+
+class SummaryState extends State<SummaryScreen> {
+  bool isExpanded = false;
+  final List<Map<String, dynamic>> nutritionData = [
+      {"name": "Fats", "amount": "17g", "dv": "42% DV"},
+      {"name": "Carbohydrates", "amount": "40g", "dv": "16% DV"},
+      {"name": "Proteins", "amount": "4g", "dv": "4% DV"},
+    ];
+
+  final List<String> recommendations = [
+      "Limit portion size",
+      "Perfect for special treats, not daily snacks"
+    ];
+
+  final List<String> alternatives = [
+      "Greek Yogurt",
+      "Fruits",
+      "Smoothies"
+    ];
+
+  final String productName = "Häagen-Dazs Ice Cream";
+
+  @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
@@ -13,7 +39,7 @@ class SummaryScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                'Häagen-Dazs Ice Cream',
+                '$productName',
                 style: TextStyle(
                   fontFamily: 'Mulish',
                   fontSize: 24,
@@ -26,8 +52,24 @@ class SummaryScreen extends StatelessWidget {
               SizedBox(height: 30),
               nutritionWarnings(2),
               SizedBox(height: 35),
-              allergenBox(),
-              // You can add more widgets here that will form the rest of your summary page
+              allergenBox(context, ['milk']),  // Displays warning with these allergens
+              SizedBox(height: 25),
+              Divider(color: Colors.grey[300], thickness: 1), // Divider before the dropdown
+              customCollapsibleNutritionDetails(),
+              Divider(color: Colors.grey[300], thickness: 1), // Divider before the dropdown
+              gradeDisplay(2),
+              Text( //bikin center
+                'Based on highlighted nutrients and additional nutritional information',
+                style: TextStyle(
+                  fontFamily: 'Mulish',
+                  fontSize: 13,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 17),
+              Divider(color: Colors.grey[300], thickness: 1),
+              showRecommendations(context, recommendations, alternatives),
             ],
           ),
         ),
@@ -42,14 +84,14 @@ class SummaryScreen extends StatelessWidget {
       children: [
         calorieInfo('330', 'cal', 'per serving'), // Calorie info for per serving
         verticalDivider(),
-        percentageInfo(), // Custom widget for daily needs with formatted text
+        percentageInfo(20), // Custom widget for daily needs with formatted text
         verticalDivider(),
         calorieInfo('870', 'cal', 'per container'), // Total calories per container
       ],
     );
   }
 
-    Widget verticalDivider() {
+  Widget verticalDivider() {
     return Container(
       height: 50, // Adjust height to match your row's content
       width: 3, // Can be thinner or thicker as per your design
@@ -94,7 +136,7 @@ class SummaryScreen extends StatelessWidget {
     );
   }
 
-  Widget percentageInfo() {
+  Widget percentageInfo(int percentage) {
     return RichText(
       text: TextSpan(
         style: TextStyle(
@@ -103,7 +145,7 @@ class SummaryScreen extends StatelessWidget {
         ),
         children: <TextSpan>[
           TextSpan(
-            text: '18% of\n', // Use \n to force the text onto the next line
+            text: '$percentage% of\n', // Use \n to force the text onto the next line
             style: TextStyle(fontSize: 17, color: Color.fromRGBO(255, 99, 0, 1), fontWeight: FontWeight.w500),
           ),
           TextSpan(
@@ -223,12 +265,172 @@ class SummaryScreen extends StatelessWidget {
     );
   }
 
-  Widget allergenBox(){
-    return Container(
-      //nanti tambah padding
-      height: 30,
-      color: Color.fromRGBO(255, 193, 103, 1),
+  Widget allergenBox(BuildContext context, List<String> allergens) {
+    String text;
+    Widget icon = SizedBox(width: 0);  // Adjusted to ensure spacing is correct even when no icon
+    Color backgroundColor = Color.fromRGBO(255, 193, 103, 1);  // Default color for no allergens
+    Color textColor = Colors.black;  // Default text color for no allergens
+
+    // Determine the text and possibly add an icon based on allergens
+    if (allergens.isEmpty) {
+      text = "This product does not contain any allergens";
+    } else {
+      text = "Warning! This product contains ${allergens.join(' and ')}";
+      icon = Padding(
+        padding: EdgeInsets.only(right: 10),  // Add padding to the right of the icon
+        child: Image.asset('assets/summary_screen/warning_sign.png', width: 17, height: 17),
       );
+      backgroundColor = Color.fromRGBO(254, 109, 2, 1);  // Bright red color for allergens present
+      textColor = Colors.white; // White text color for better contrast on red background
+    }
+
+    return Center(
+      child: Container(
+        height: 35,
+        padding: EdgeInsets.symmetric(horizontal: 10),  // Padding for text and icon
+        width: MediaQuery.of(context).size.width * 0.8,  // 80% of screen width
+        decoration: BoxDecoration(
+          color: backgroundColor,  // Dynamic background color
+          borderRadius: BorderRadius.circular(8),  // Rounded corners
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,  // Center align items horizontally
+          crossAxisAlignment: CrossAxisAlignment.center,  // Center align items vertically
+          children: [
+            icon,
+            Expanded(
+              child: Text(
+                text,
+                textAlign: TextAlign.center,  // Center align text horizontally
+                style: TextStyle(
+                  color: textColor,  // Dynamic text color
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget customCollapsibleNutritionDetails() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListTile(
+          title: Text(
+            "Nutrition Details",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+          ),
+          trailing: IconButton(
+            icon: Icon(isExpanded ? Icons.expand_less : Icons.expand_more),
+            onPressed: () {
+              setState(() {
+                isExpanded = !isExpanded;
+              });
+            },
+          ),
+        ),
+        Visibility(
+          visible: isExpanded,
+          child: Column(
+            children: nutritionData.map((item) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 6.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      flex: 3, // Adjust flex ratio for name column
+                      child: Text(
+                        item['name']!,
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2, // Adjust flex ratio for amount column to align amounts
+                      child: Text(
+                        item['amount']!,
+                        textAlign: TextAlign.center, // Center align text
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2, // Adjust flex ratio for dv column
+                      child: Text(
+                        item['dv']!,
+                        textAlign: TextAlign.right, // Right align text
+                        style: TextStyle(fontSize: 15, color: Colors.grey),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+            // SizedBox(height: 10),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget gradeDisplay(int score) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      alignment: Alignment.center,
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+            fontFamily: 'Mulish',
+          ),
+          children: [
+            TextSpan(text: 'Grade\n', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22)),
+            TextSpan(text: '$score ', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 50, color: Color.fromRGBO(255, 28, 82, 1))),
+            TextSpan(text: '/ 5', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 30, color: Color.fromRGBO(247, 145, 1, 1)))
+          ],
+        ),
+      ),
+    );
+  }
+  
+  Widget showRecommendations(BuildContext context, List<String> recommendations, List<String> alternatives) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 12),
+        Text(
+          'Recommendations',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.black),
+        ),
+        SizedBox(height: 5),
+        ...recommendations.map((item) => Padding(
+          padding: const EdgeInsets.only(bottom: 4),
+          child: Text(
+            '• $item',
+            style: TextStyle(fontSize: 15, color: Colors.black),
+          ),
+        )),
+        SizedBox(height: 14),
+        Text(
+          'Alternatives:',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700, color: Colors.black),
+        ),
+        SizedBox(height: 2),
+        Padding(
+          padding: const EdgeInsets.only(top: 4),
+          child: Text(
+            alternatives.join(', '), // Joining alternatives with commas
+            style: TextStyle(fontSize: 15, color: Colors.black),
+          ),
+        ),
+        SizedBox(height: 20)
+      ],
+    );
   }
 }
 

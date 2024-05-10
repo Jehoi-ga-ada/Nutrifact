147a8b9f7bac14fa4b7069e0d68867b46803134f
Jehoiada Wong <sayajehoiada@gmail.com>
2024-05-10 20:17:29 +0700

    sidebar design finihed

--- lib/screens/sidebar.dart
+++ lib/screens/sidebar.dart
@@ -33,37 +33,45 @@ class _SideBarState extends State<SideBar> {
                     onTap: () => Navigator.pop(context),
                   ),
                 ),
-
-                UserAccountsDrawerHeader(
-                  accountName: const Padding(
-                    padding: EdgeInsets.only(
-                      bottom: 10,
-                    ),
-                    child: Text(
-                      "User Masbro",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 22,
-                        fontFamily: 'Mulish'
-                      ),
-                    ),
+                
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.only(
+                    top: 0.02*height,
+                    bottom: 0.01*height, 
+                    left: 0.04*width
                   ),
-                  accountEmail: null,
-                  currentAccountPicture: CircleAvatar(
-                    child: ClipOval(
-                      child: Image.network(
-                        'https://i.pinimg.com/736x/86/d3/e2/86d3e24b8647e52cc6c815a52ff6e445.jpg',
-                        fit: BoxFit.cover,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 45, // half of currentAccountPictureSize
+                        backgroundColor: Colors.transparent,
+                        child: ClipOval(
+                          child: Image.network(
+                            'https://i.pinimg.com/736x/86/d3/e2/86d3e24b8647e52cc6c815a52ff6e445.jpg',
+                            fit: BoxFit.cover,
+                            width: 90, // currentAccountPictureSize.width
+                            height: 90, // currentAccountPictureSize.height
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  currentAccountPictureSize: const Size.square(90),
-                  decoration: const BoxDecoration(
-                    color: Colors.transparent,
-                    border: null,
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 10, top: 5),
+                        child: Text(
+                          "User Masbro",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22,
+                            fontFamily: 'Mulish',
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                
+
                 SizedBox(height: 0.01*height),
 
                 const ListTile(

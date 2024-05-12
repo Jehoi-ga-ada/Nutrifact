83ef5010470701a83fd8747a1e0cf7be3e09281b
Jehoiada Wong <sayajehoiada@gmail.com>
2024-05-12 18:43:42 +0700

    loading_screen init

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

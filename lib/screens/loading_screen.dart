558db77ee908a06a7259c1792375500da388de52
Jehoiada Wong <sayajehoiada@gmail.com>
2024-05-12 21:01:15 +0700

    loading screen finished

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,8 +9,55 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+    return Scaffold(  
+      backgroundColor:const Color.fromRGBO(175, 177, 182, 1),
+      body: Stack(
+        children: [
+          SafeArea(
+            minimum: EdgeInsets.only(
+              bottom: 0.07 * height,
+            ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/loading_screen/processing_image.png',
+                height: 200,
+                width: 200,
+              ),
+            ),
+          ),
+
+          SafeArea(
+            minimum: EdgeInsets.only(
+              bottom: 0.07 * height,
+            ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: 0.25 * height, 
+                ),
+                child: const Text(
+                  "Processing Image . . .",
+                  style: TextStyle(
+                    fontFamily: 'Mulish',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      )
+    );
   }
 }

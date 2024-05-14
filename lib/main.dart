384ef9a0e2f6b5a230692f5f12e7d49fe18a24e8
Jehoiada Wong <sayajehoiada@gmail.com>
2024-05-14 15:27:07 +0700

    Changed to HomeScreen as default

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:nutrifact/screens/profile_form.dart';
 import 'package:nutrifact/screens/home_screen.dart';
-import 'package:nutrifact/screens/profile.dart';
-import 'package:nutrifact/screens/summary_screen.dart';
-// import 'package:flutter_native_splash/flutter_native_splash.dart';
-// import 'package:nutrifact/screens/splash_screen.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +17,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ProfileForm(),
+      home: HomeScreen(
+        cameras: cameras,
+      ),
     );
   }
 }
